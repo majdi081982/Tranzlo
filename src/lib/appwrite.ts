02c20f43043da75edaf43f5eb0830425e74a934f
[elmony] <majdiaboalila7@gmail.com>
2026-03-11 14:52:09 +0200

    [elmony] Updated the Jobs collection ID in the Appwrite configuration. - wrote 1 file(s)

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -10,7 +10,7 @@ export const storage = new Storage(client);
 
 // Database and Collection IDs
 export const DATABASE_ID = '69b15c880036f57c2909'; 
-export const COLLECTION_JOBS = '67d018d00010c79f38e0'; // Note: Ensure this collection exists in the new DB
+export const COLLECTION_JOBS = '8nfhi86kwwlmgsgibk7k2jgtsm8shboq'; 
 export const COLLECTION_PROFILES = 'profiles';
 
 export { client, ID };
